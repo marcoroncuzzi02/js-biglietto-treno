da41242859fd5010ec13b54a59fb25dcfc8adc92
marcoroncuzzi02 <marcoroncuzzi02@gmail.com>
2025-01-09 16:09:17 +0100

    END

--- main.js
+++ main.js
@@ -6,17 +6,21 @@
 // - L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
 
 // considero l'età e i kilometri come variabili e il prezzo come constante che deriva dalla moltiplicazione dei kilometri per una constante
-let età = 25
-let kilometri = 50
-const prezzo = 0.21 * kilometri
+let età = 66
+let kilometri = 100
+let prezzo = 0.21 * kilometri
+let discount = 0;
 
 // pongo le condizioni per lo sconto del prezzo del biglietto
 
-if (età < 18) {
-    const prezzo = prezzo - (prezzo * 0.20)
-    console.log(prezzo);
+if (età < 18){
+    discount = prezzo * 0.20   
 }
 else if (età > 65){
-    const prezzo = prezzo - (prezzo * 0.40)
-    console.log(prezzo);
+    discount = prezzo * 0.40
 }
+
+prezzo = prezzo - discount
+console.log(`il prezzo è di ${prezzo}$`);
+
+//INPUT -> ELABORAZIONE/CONTROLLO -> OUTPUT
